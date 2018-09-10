dc02a6cec8ceb356fa9fefea411b6e85a83b8498
McKay Nilsson <mckay.nilsson@gmail.com>
2018-09-10 12:36:42 -0600

    react 2 final commit

--- src/components/EditToggle.js
+++ src/components/EditToggle.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-export default function EditToggle() {
+export default function EditToggle(props) {
   return (
-    <select className="dropDownContainer ml0">
+    <select onChange={e => props.update(e.target.value)} className="dropDownContainer ml0">
       <option value="true"> Allow Edit </option>
       <option value="false"> Disable Edit </option>
     </select>
--- src/components/FamilyChanger.js
+++ src/components/FamilyChanger.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-export default function FamilyChanger() {
+export default function FamilyChanger(props) {
   return (
-    <select className="dropDownContainer">
+    <select onChange={e => props.update(e.target.value)} disabled={props.allowEdit === 'false'} className="dropDownContainer">
       <option value="monospace"> Monospace </option>
       <option value="arial"> Arial </option>
       <option value="courier"> Courier </option>
